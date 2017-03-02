00be4de8169654d38548c60c4d7da3222c3a365a
Liju Abraham <abrahamliju@live.com>
2017-03-02 21:41:34 +0000

    added login , register routes

--- routes/users.js
+++ routes/users.js
@@ -2,26 +2,48 @@
 
 const express = require('express');
 const router  = express.Router();
+const md5 = require('md5');
 
 module.exports = (knex) => {
 
-  // router.get("/", (req, res) => {
-  //   knex
-  //     .select("*")
-  //     .from("users")
-  //     .then((results) => {
-  //       //res.json(results);
-  //   });
-  // });
+  function createAvatar(email){
+    const avatarUrlPrefix = `https://vanillicon.com/${md5(email)}`;
+    const avatars = {
+      small:   `${avatarUrlPrefix}_50.png`,
+      regular: `${avatarUrlPrefix}.png`,
+      large:   `${avatarUrlPrefix}_200.png`
+    }
+    console.log(avatars);
+    return avatars.small;
+  }
 
   router.get("/", (req, res) => {
     res.render("login.ejs");
   })
 
-  router.post("/login", (res, req) => {
-    console.log("login post");
-    res.redirect("/")
+  router.post("/login", (req, res) => {
+    console.log(req.body.userEmail, req.body.userPassword);
+    knex('users').where({
+      email: req.body.userEmail,
+      password: req.body.userPassword
+    })
+      .asCallback(function(err, rows){
+        if(rows.length > 0){
+          res.redirect("/");
+        } else {
+          res.send("Username or password do not exist");
+        }
+      })
   })
+
+  router.post("/register", (req, res) => {
+    knex('users').insert([{name: req.body.name, email: req.body.reg_email, password: req.body.reg_password, avatar: createAvatar(req.body.reg_email)
+    }])
+    .then(function(resp) {
+        res.redirect("/");
+    })
+  })
+
 
   router.get("/:id/edit", (req, res) => {
     res.render("edit.ejs");
