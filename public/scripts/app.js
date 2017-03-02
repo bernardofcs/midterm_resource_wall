6c1a37bbb83cafc63102e92f81ff062839af1871
Bernardo Siqueira <bernardofcs@hotmail.com>
2017-03-02 21:44:51 +0000

    added initial html markup generating

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,30 @@
 $(document).ready(function (){
+
   function loadResources(){
     $.ajax({
       method: "GET",
       url: "/resources"
     }).done((resources) => {
+      $('#main-container').html('');
       for(resource of resources) {
-
+        let $resource = createResourceElement(resource);
+        $('#main-container').append($resource);
       }
+
     });
   }
+
+  function createResourceElement(resource){
+    let resourceFormat = $(`<div class="card">
+      <div class="card-block">
+        <h4 class="card-title">${resource.description}</h4>
+        <p class="card-text">URL:  <iframe src="${resource.url}"></iframe></p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>`);
+
+    return resourceFormat;
+  }
+  loadResources();
+
 });
