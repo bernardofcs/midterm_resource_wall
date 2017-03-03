c9eb7f90e4badc51677813252edd68113b49c141
Alex-Joseph <alex.cheng@ryerson.ca>
2017-03-03 19:09:58 +0000

    updated css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,7 +18,7 @@ $(document).ready(function (){
     let resourceFormat = $(`<div class="card">
       <div class="card-block">
         <h4 class="card-title">${resource.description}</h4>
-        <p class="card-text">URL:  <iframe src="${resource.url}"></iframe></p>
+        <p class="card-text">URL:  <iframe class="url-image" src="${resource.url}"></iframe></p>
         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
       </div>
     </div>`);
